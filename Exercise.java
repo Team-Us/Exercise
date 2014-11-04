4ad1dc9f0684351e532399b63b13c3f415b0b354
HerbyJ <dunn.ryan.01@gmail.com>
2014-11-03 20:45:08 -0700

    Exercise
    Exercise class with private fields and private methods as in our class
    diagrams

---
+++ Exercise.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Exercise 
+{
+	private int eld;
+	private String topic;
+	private String exerciseName;
+	private ArrayList<String> answers;
+	private ArrayList<String> hints;
+	private int difficulty;
+	
+	private boolean checkAnswers(String a)
+	{
+		return false;
+	}
+	
+	private String showHint(int a)
+	{
+		return "I am returning this sucka!";
+	}
+}
